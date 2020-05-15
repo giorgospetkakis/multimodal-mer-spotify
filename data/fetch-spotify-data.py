2e5c92a0b0373424af825720c298dbc4df5280e5
Giorgos Petkakis <giorgospetkakis@users.noreply.github.com>
2020-05-15 16:27:50 +0200

    Fix cross-referencing problem and consolidate datasets

--- data/fetch-spotify-data.py
+++ data/fetch-spotify-data.py
@@ -8,15 +8,15 @@ import re
 URL_ID = "https://api.spotify.com/v1/search?q="
 URL_FEATURES = "https://api.spotify.com/v1/audio-features/"
 
-PARAM = "&type=track&market=US&limit=10&offset=5"
+PARAM = "&type=track&limit=10&offset=5"
 ACC = "Accept: application/json"
 TYP = "Content-Type: application/json"
-AUTH = "AUTH_KEY_HERE" ## ENTER AUTH KEY ##
+AUTH = "Authorization: Bearer BQAigNqnUXgZwMNHD_2Wp37g---fd8znffsUpkvO3TZPqDIu2136dbC3oQSM2qt5x_2aROgJgOzOSL1uui3tAuSoRmqgT0Hzi8eBjyRUqXBQnM1VRan28squuEI_N-09aW-PQAj2vt0Y" ## ENTER AUTH KEY ##
 
 
 # Extract the tracks from the csv file
 entries = []
-with open('data\MoodyLyrics4Q\MoodyLyrics4Q.csv', newline='', encoding='utf-8') as f:
+with open('MoodyLyrics4Q/MoodyLyrics4Q.csv', newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
         entries += [row]
@@ -27,21 +27,23 @@ queries = [re.sub(r" ", "%20", e[1]) + "%20"+ re.sub(r"\s", "%20", e[2]) for e i
 
 # Run Queries
 features = []
-for q in queries:
+idx = []
+for i, q in enumerate(queries):
     # Get song ids
-    res = subprocess.check_output(f'curl -X "GET" "{URL_ID}{q}{PARAM}" -H "{ACC}" -H "{TYP}" -H "{AUTH}"')
+    res = subprocess.check_output(f"curl -X \"GET\" \"{URL_ID}{q}{PARAM}\" -H \"{ACC}\" -H \"{TYP}\" -H \"{AUTH}\"", shell=True)
     filtered = re.search(r"https:\/\/api\.spotify\.com\/v1\/tracks\/(.*?)\"", str(res))
     if (filtered == None):
         continue
     # Get song features
-    feat = subprocess.check_output(f'curl -X "GET" "{URL_FEATURES}{filtered.group(1)}" -H "{ACC}" -H "{TYP}" -H "{AUTH}"')
+    feat = subprocess.check_output(f'curl -X "GET" "{URL_FEATURES}{filtered.group(1)}" -H "{ACC}" -H "{TYP}" -H "{AUTH}"', shell=True)
     serialized = json.loads(feat, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
     features += [serialized]
+    idx += [i]
 
-header_line = "id,duration_ms,key,tempo,time_signature,danceability,energy,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence\n"
-append = open("data/SpotifyData.csv", "a")
+header_line = "id,artist,title,mood,duration_ms,key,tempo,time_signature,danceability,energy,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence\n"
+append = open("SpotifyData-1.csv", "a")
 append.writelines(header_line)
-for f in features:
-    string = str(f.id) + "," + str(f.duration_ms) + "," + str(f.key) + "," + str(f.tempo) + "," + str(f.time_signature) + "," + str(f.danceability) + "," + str(f.energy)  + "," + str(f.loudness) + "," + str(f.mode) + "," + str(f.speechiness) + "," + str(f.acousticness) + "," + str(f.instrumentalness) + "," + str(f.liveness) + "," + str(f.valence) + "\n"
+for j, f in enumerate(features):
+    string = str(f.id) + "," + entries[idx[j]+1][1] + "," + entries[idx[j]+1][2] + "," + entries[idx[j]+1][3] + "," + str(f.duration_ms) + "," + str(f.key) + "," + str(f.tempo) + "," + str(f.time_signature) + "," + str(f.danceability) + "," + str(f.energy)  + "," + str(f.loudness) + "," + str(f.mode) + "," + str(f.speechiness) + "," + str(f.acousticness) + "," + str(f.instrumentalness) + "," + str(f.liveness) + "," + str(f.valence) + "\n"
     append.writelines(string)
 append.close()
