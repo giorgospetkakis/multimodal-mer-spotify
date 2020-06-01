1a6d35da1135e241cc1a75d51ad37573bff7a869
Giorgos Petkakis <giorgospetkakis@users.noreply.github.com>
2020-06-02 00:56:01 +0200

    Weekend madness - we're done!

--- src/preprocess-data.py
+++ src/preprocess-data.py
@@ -5,19 +5,21 @@ import csv
 
 # Import csv file 
 df = pd.read_csv("data/raw/SpotifyData.csv", sep=",")
-df = df[["id","artist","title","key","mode","mood","duration_ms","tempo","danceability","energy","loudness","speechiness","acousticness","instrumentalness","liveness","valence"]]
-categ = np.array(df.iloc[:,3:5]) 
-raw = np.array(df.iloc[:, 5:])
+df = df[["id","artist","title","key","mode","time_signature","mood", "duration_ms","tempo","danceability","energy","loudness","speechiness","acousticness","instrumentalness","liveness","valence"]]
+
+categ = np.array(df.iloc[:,3:6]) 
+raw = np.array(df.iloc[:, 6:])
 
 # Standardize data
 scaler = preprocessing.StandardScaler()
 scaled_df = scaler.fit_transform(raw[:,1:])
+
 scaled_df = np.hstack((categ, scaled_df))
 scaled_df = np.hstack((scaled_df, np.zeros((scaled_df.shape[0], 4))))
 
+print()
 # Create binary columns
 for i, row in enumerate(raw):
-    print(row)
     if (row[0] == 'happy'):
         scaled_df[i,-4] = 1
     if (row[0] == 'sad'):
@@ -29,5 +31,5 @@ for i, row in enumerate(raw):
 
 # Rename columns and clean up data frame
 print(scaled_df)
-scaled_df = pd.DataFrame(scaled_df, columns=["key", "mode"] + list(df.columns)[6:] + ["is_happy", "is_sad", "is_angry", "is_relaxed"], index=df["id"])
+scaled_df = pd.DataFrame(scaled_df, columns=["key", "mode", "time_signature"] + list(df.columns)[7:] + ["is_happy", "is_sad", "is_angry", "is_relaxed"], index=df["id"])
 scaled_df.to_csv('data/preprocessed/spotify-data-preprocessed.csv')
